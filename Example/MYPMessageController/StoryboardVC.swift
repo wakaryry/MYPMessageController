18dc848292f77764dd1f1748b0366321754a3c69
wakary <redoume@163.com>
2018-05-14 15:38:11 +0800

    update

---
+++ Example/MYPMessageController/StoryboardVC.swift
@@ -0,0 +1,36 @@
+//
+//  StoryboardVC.swift
+//  MYPMessageController_Example
+//
+//  Created by wakary redou on 2018/5/14.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import MYPMessageController
+
+class StoryboardVC: MYPMessageController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
