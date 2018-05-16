4408f1dbc98c73a49025e7e895f81ae191d4afc4
wakary <redoume@163.com>
2018-05-16 17:46:48 +0800

    update

--- MYPMessageController/Classes/MYPInputAccessoryView.swift
+++ MYPMessageController/Classes/MYPInputAccessoryView.swift
@@ -10,5 +10,34 @@ import UIKit
 
 public class MYPInputAccessoryView: UIView {
     /* The system keyboard view used as reference. */
-    private(set) weak var keyboardViewProxy: UIView?
+    weak var keyboardViewProxy: UIView? {
+        return keyboardViewProxyHelper
+    }
+    
+    lazy private var keyboardViewProxyHelper: UIView? = {
+        let winds = UIApplication.shared.windows
+        
+        for w in winds.reversed() {
+            let boardView = self.findKeyboard(in: w)
+            if boardView != nil {
+                return boardView
+            }
+        }
+        return nil
+    }()
+    
+    private func findKeyboard(in view: UIView) -> UIView? {
+        for sub in view.subviews {
+            if (strstr(object_getClassName(sub), "UIKeyboard") != nil) {
+                return sub
+            }
+            else {
+                let temp = self.findKeyboard(in: sub)
+                if temp != nil {
+                    return temp
+                }
+            }
+        }
+        return nil
+    }
 }
